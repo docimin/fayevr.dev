61790aca5378e0abc50b69464073e3a0ac439c33
Faye <dominic@stilma.net>
2022-10-29 01:17:03 +0200

    test

---
+++ next-seo.config.js
@@ -0,0 +1,35 @@
+const defaultSEOConfig = {
+  title: "Faye | Fullstack Developer",
+  description:
+    "10th grader • Fullstack Web Developer • Web3 Enthusiast • Blogger • Tech Enthusiast",
+  cannonical: "https://fayevr.dev/",
+  openGraph: {
+    type: "website",
+    url: "https://fayevr.dev/",
+    site_name: "FayeVR",
+    title: "FayeVR | Fullstack Developer",
+    description:
+      "10th grader • Fullstack Web Developer • Web3 Enthusiast • Blogger • Tech Enthusiast",
+    images: [
+      {
+        url: "https://res.cloudinary.com/anishde12020/image/upload/v1654360780/Blogfolio/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Faye's Portfolio Site's Image",
+      },
+    ],
+    profile: {
+      firstName: "Dominic",
+      lastName: "S.",
+      username: "FayeVR",
+      gender: "male",
+    },
+  },
+  twitter: {
+    handle: "@fayeofficial_",
+    site: "@fayevr",
+    cardType: "summary_large_image",
+  },
+};
+
+export default defaultSEOConfig;
