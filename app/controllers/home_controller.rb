ac7428554f9d3e1be740ed725dad378da45f843a
Faye <dominic@stilma.net>
2023-01-23 18:15:38 +0100

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,13 @@
-require 'date'
-
 class HomeController < ApplicationController
   def index
   end
   def age
+    require 'date'
     today = Date.today
     birthday = Date.new(2000, 8, 17)
-    @age = today.year - birthday.year
-  end
-  def something
-    @something1 = "test"
-    @something2 = puts @something1
+    @age = today.year - birthday
+    if @age == nil
+      puts "Hello!"
+    end
   end
 end
