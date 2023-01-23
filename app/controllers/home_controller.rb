e04d5b707679075ee3141335f6b7ee0cd6c22a7d
Faye <dominic@stilma.net>
2023-01-23 17:11:17 +0100

    hmm

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
+require 'date'
+
 class HomeController < ApplicationController
   def index
+  end
+  def age
+    today = Date.today
+    birthday = Date.new(2000, 8, 17)
+    @age = today.year - birthday.year
+    @age -= 1 if today.month < birthday.month || (today.month == birthday.month && today.day < birthday.day)
+  end
+
+  def about
+    today = Date.today
+    birthday = Date.new(2000, 8, 17)
+    @about = today.year - birthday.year
+    p @about
   end
 end
