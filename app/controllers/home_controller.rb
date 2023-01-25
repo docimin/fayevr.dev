05d3623830752edfe83bb7ef03bbf60a079eb3f8
Faye <dominic@stilma.net>
2023-01-25 14:33:57 +0100

    .. it shows my age!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,8 @@
 class HomeController < ApplicationController
   def index
-  end
-  def age
     require 'date'
     today = Date.today
     birthday = Date.new(2000, 8, 17)
-    @age = today.year - birthday
-    if @age == nil
-      puts "Hello!"
-    end
+    @age = today.year - birthday.year - ((today.month > birthday.month || (today.month == birthday.month && today.day >= birthday.day)) ? 0 : 1)
   end
 end
