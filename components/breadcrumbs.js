5ed882b364b17c67d8555926d60d854b3889454d
Faye <dominic@stilma.net>
2023-06-28 16:35:45 +0200

    use <Link> instead of <a>

--- components/breadcrumbs.js
+++ components/breadcrumbs.js
@@ -1,10 +1,12 @@
+import Link from "next/link"
+
 export default function Example() {
   return (
     <div className="pt-8">
       <nav aria-label="breadcrumb"> 
         <ol className="flex space-x-2">
-          <li><a href="#" className="after:content-['/'] after:ml-2 text-gray-600 hover:text-purple-700">~</a></li>
-          <li><a href="#" className="after:content-['/'] after:ml-2 text-gray-600 hover:text-purple-700">projects</a></li>
+          <li><Link href="#" className="after:content-['/'] after:ml-2 text-gray-600 hover:text-purple-700">~</Link></li>
+          <li><Link href="#" className="after:content-['/'] after:ml-2 text-gray-600 hover:text-purple-700">projects</Link></li>
           <li className="text-purple-700" aria-current="page">alyx-bot</li>
         </ol>
       </nav>
