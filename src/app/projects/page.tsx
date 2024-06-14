e5acb04465ca33581276e3e06d9c267652bc23e7
faye <dominic@stilma.net>
2024-06-14 11:13:56 +0200

    sorting projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -82,69 +82,77 @@ export default async function Projects() {
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray/50 dark:divide-white/20">
-                {projects.map((item) => (
-                  <tr key={item.name}>
-                    <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                      <div className="flex items-center gap-x-4">
-                        <img
-                          src={item.imageId}
-                          alt=""
-                          className="h-8 w-8 rounded-full bg-gray-800"
-                        />
-                        <div className="truncate text-sm font-medium leading-6 dark:text-white text-black">
-                          {item.name}
+                {projects
+                  .sort(
+                    (a, b) =>
+                      new Date(b.$createdAt).getTime() -
+                      new Date(a.$createdAt).getTime(),
+                  )
+                  .map((item) => (
+                    <tr key={item.name}>
+                      <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
+                        <div className="flex items-center gap-x-4">
+                          <img
+                            src={item.imageId}
+                            alt={item.name}
+                            className="h-8 w-8 rounded-full bg-secondary-foreground"
+                          />
+                          <div className="truncate text-sm font-medium leading-6 dark:text-white text-black">
+                            {item.name}
+                          </div>
                         </div>
-                      </div>
-                    </td>
-                    <td className="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8">
-                      <div className="flex gap-x-3">
-                        <div className="font-mono text-sm leading-6 dark:text-gray-400 text-gray-800">
-                          <Link
-                            href={item.gitRepo}
-                            className="hover:text-primary"
-                            target="_blank"
+                      </td>
+                      <td className="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8">
+                        <div className="flex gap-x-3">
+                          <div className="font-mono text-sm leading-6 dark:text-gray-400 text-gray-800">
+                            <Link
+                              href={item.gitRepo}
+                              className="hover:text-primary"
+                              target="_blank"
+                            >
+                              Link
+                            </Link>
+                          </div>
+                          <div className="rounded-md bg-gray-700/40 px-2 py-1 text-xs font-medium dark:text-gray-400 text-gray-800 ring-1 ring-inset ring-white/10">
+                            {item.branch}
+                          </div>
+                        </div>
+                      </td>
+                      <td className="py-4 pl-0 pr-4 text-sm leading-6 sm:pr-8 lg:pr-20">
+                        <div className="flex items-center justify-end gap-x-2 sm:justify-start">
+                          <time className="dark:text-gray-400 text-gray-800 sm:hidden">
+                            {item.$createdAt}
+                          </time>
+                          <div
+                            className={cn(
+                              statuses[item.status],
+                              "flex-none rounded-full p-1",
+                            )}
                           >
-                            Link
-                          </Link>
+                            <div className="h-1.5 w-1.5 rounded-full bg-current" />
+                          </div>
+                          <div className="hidden dark:text-white text-black sm:block">
+                            {item.status}
+                          </div>
                         </div>
-                        <div className="rounded-md bg-gray-700/40 px-2 py-1 text-xs font-medium dark:text-gray-400 text-gray-800 ring-1 ring-inset ring-white/10">
-                          {item.branch}
-                        </div>
-                      </div>
-                    </td>
-                    <td className="py-4 pl-0 pr-4 text-sm leading-6 sm:pr-8 lg:pr-20">
-                      <div className="flex items-center justify-end gap-x-2 sm:justify-start">
-                        <time className="dark:text-gray-400 text-gray-800 sm:hidden">
-                          {item.$createdAt}
-                        </time>
-                        <div
-                          className={cn(
-                            statuses[item.status],
-                            "flex-none rounded-full p-1",
+                      </td>
+                      <td className="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8 dark:text-gray-400 text-gray-800">
+                        <time>
+                          {new Date(item.$createdAt).toLocaleDateString(
+                            "en-GB",
                           )}
+                        </time>
+                      </td>
+                      <td className="hidden py-4 pl-0 pr-4 text-right text-sm leading-6 text-primary sm:table-cell sm:pr-6 lg:pr-8">
+                        <Link
+                          href={item.customUrl || item.gitRepo}
+                          className="hover:text-primary"
                         >
-                          <div className="h-1.5 w-1.5 rounded-full bg-current" />
-                        </div>
-                        <div className="hidden dark:text-white text-black sm:block">
-                          {item.status}
-                        </div>
-                      </div>
-                    </td>
-                    <td className="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8 dark:text-gray-400 text-gray-800">
-                      <time>
-                        {new Date(item.$createdAt).toLocaleDateString("en-GB")}
-                      </time>
-                    </td>
-                    <td className="hidden py-4 pl-0 pr-4 text-right text-sm leading-6 text-primary sm:table-cell sm:pr-6 lg:pr-8">
-                      <Link
-                        href={item.customUrl || item.gitRepo}
-                        className="hover:text-primary"
-                      >
-                        Visit page --&gt;
-                      </Link>
-                    </td>
-                  </tr>
-                ))}
+                          Visit page --&gt;
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
               </tbody>
             </table>
           </div>
