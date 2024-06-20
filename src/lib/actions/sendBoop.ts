17d822b54a4153ab34447e507283e22a1d9d7550
Faye <dominic@stilma.net>
2024-06-20 23:29:51 +0200

    add timer

--- src/lib/actions/sendBoop.ts
+++ src/lib/actions/sendBoop.ts
@@ -1,11 +1,22 @@
-"use server";
-import { databases } from "@/app/appwrite-server";
+'use server'
+import { databases } from '@/app/appwrite-server'
+
+let lastBoopTime = 0
+const cooldown = 200 // cooldown period in milliseconds
 
 export async function sendBoop() {
-  const data = await databases.getDocument("main", "counters", "mainBoop");
-  const count = data.boop;
+  const now = Date.now()
+  if (now < lastBoopTime + cooldown) {
+    // If it's too soon to send another boop, return without doing anything
+    return
+  }
 
-  return await databases.updateDocument("main", "counters", "mainBoop", {
+  const data = await databases.getDocument('main', 'counters', 'mainBoop')
+  const count = data.boop
+
+  lastBoopTime = now // update the last boop time
+
+  return await databases.updateDocument('main', 'counters', 'mainBoop', {
     boop: count + 1,
-  });
+  })
 }
