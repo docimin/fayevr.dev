4b7cb3de99719aa1533b61c85a34a71b80ff24d3
Faye <dominic@stilma.net>
2024-06-20 23:27:06 +0200

    add context menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,40 @@
-import "../../css/globals.css";
-import { Inter } from "next/font/google";
-import { Providers } from "@/components/providers";
-import ClickSound from "@/hooks/click";
+import '../../css/globals.css'
+import { Inter } from 'next/font/google'
+import { Providers } from '@/components/providers'
+import ClickSound from '@/hooks/click'
+import ContextMenuProvider from '@/components/contextMenu'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: {
-    default: "Faye | Portfolio",
-    template: "%s | Faye",
+    default: 'Faye | Portfolio',
+    template: '%s | Faye',
   },
   description:
     "Faye's personal portfolio website. A place to showcase my projects and skills.",
   keywords: [
-    "faye",
-    "fayevr",
-    "fayevr.dev",
-    "faye vr",
-    "portfolio",
-    "website",
-    "personal",
-    "personal website",
-    "personal portfolio",
+    'faye',
+    'fayevr',
+    'fayevr.dev',
+    'faye vr',
+    'portfolio',
+    'website',
+    'personal',
+    'personal website',
+    'personal portfolio',
   ],
   openGraph: {
-    title: "Faye | Portfolio",
+    title: 'Faye | Portfolio',
     description:
       "Faye's personal portfolio website. A place to showcase my projects and skills.",
-    url: "https://fayevr.dev",
-    images: "icon-512.png",
-    type: "website",
-    locale: "en_US",
+    url: 'https://fayevr.dev',
+    images: 'icon-512.png',
+    type: 'website',
+    locale: 'en_US',
   },
-  metadataBase: new URL("https://fayevr.dev"),
-};
+  metadataBase: new URL('https://fayevr.dev'),
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -42,10 +43,12 @@ export default function RootLayout({ children }) {
         className={`${inter.className} flex min-h-full bg-white antialiased dark:bg-black`}
       >
         <Providers>
-          <div className="w-full">{children}</div>
+          <ContextMenuProvider>
+            <div className="w-full">{children}</div>
+          </ContextMenuProvider>
         </Providers>
         <ClickSound />
       </body>
     </html>
-  );
+  )
 }
---
+++ src/components/contextMenu.tsx
@@ -0,0 +1,45 @@
+'use client'
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuTrigger,
+} from '@/components/ui/context-menu'
+import { SiGithub } from '@icons-pack/react-simple-icons'
+import Image from 'next/image'
+import * as React from 'react'
+import Link from 'next/link'
+
+export default function ContextMenuProvider({ children }) {
+  return (
+    <ContextMenu>
+      <ContextMenuTrigger asChild>{children}</ContextMenuTrigger>
+      <ContextMenuContent className="w-64">
+        <Link
+          href={'https://github.com/docimin/cloudnet-webinterface'}
+          passHref
+          target={'_blank'}
+        >
+          <ContextMenuItem inset>
+            GitHub
+            <ContextMenuShortcut>
+              <SiGithub size={16} />
+            </ContextMenuShortcut>
+          </ContextMenuItem>
+        </Link>
+        <ContextMenuSeparator />
+        <Link href={'/'} passHref>
+          <ContextMenuItem inset>Home</ContextMenuItem>
+        </Link>
+        <Link href={'/about'} passHref>
+          <ContextMenuItem inset>About</ContextMenuItem>
+        </Link>
+        <Link href={'/projects'} passHref>
+          <ContextMenuItem inset>Projects</ContextMenuItem>
+        </Link>
+      </ContextMenuContent>
+    </ContextMenu>
+  )
+}
