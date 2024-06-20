355eb6da823770857e2ef8f804a61497e9e92363
Faye <dominic@stilma.net>
2024-06-20 23:31:54 +0200

    add spooky category

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,52 +1,52 @@
-"use client";
-import { Fragment, Key, useState } from "react";
-import { Combobox, Dialog, Transition } from "@headlessui/react";
+'use client'
+import { Fragment, Key, useState } from 'react'
+import { Combobox, Dialog, Transition } from '@headlessui/react'
 
 const items = [
-  { id: 1, name: ">about", category: "faye", url: "/about" },
-  { id: 2, name: ">projects", category: "faye", url: "/projects" },
+  { id: 1, name: '>about', category: 'faye', url: '/about' },
+  { id: 2, name: '>projects', category: 'faye', url: '/projects' },
   {
     id: 3,
-    name: "#suggestions-bot",
-    category: "Projects",
-    url: "/projects/suggestions-bot",
+    name: '#suggestions-bot',
+    category: 'Projects',
+    url: '/projects/suggestions-bot',
   },
-  { id: 4, name: "#alyx-bot", category: "Projects", url: "/projects/alyx-bot" },
+  { id: 4, name: '#alyx-bot', category: 'Projects', url: '/projects/alyx-bot' },
   {
     id: 5,
-    name: "?github",
-    category: "Help",
-    url: "https://github.com/docimin/",
+    name: '?github',
+    category: 'Help',
+    url: 'https://github.com/docimin/',
   },
-  { id: 6, name: "doom", category: "Help", url: "/doom" },
-];
+  { id: 6, name: 'doom', category: 'Spooky!', url: '/doom' },
+]
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ')
 }
 
 export default function MenuComponent({ open, setOpen }) {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('')
 
   const filteredItems =
-    query === ""
+    query === ''
       ? []
       : items.filter((item) => {
-          return item.name.toLowerCase().includes(query.toLowerCase());
-        });
+          return item.name.toLowerCase().includes(query.toLowerCase())
+        })
 
   const groups = filteredItems.reduce((groups, item) => {
     return {
       ...groups,
       [item.category]: [...(groups[item.category] || []), item],
-    };
-  }, {});
+    }
+  }, {})
 
   return (
     <Transition.Root
       show={open}
       as={Fragment}
-      afterLeave={() => setQuery("")}
+      afterLeave={() => setQuery('')}
       appear
     >
       <Dialog as="div" className="relative z-10" onClose={setOpen}>
@@ -83,7 +83,7 @@ export default function MenuComponent({ open, setOpen }) {
                   />
                 </div>
 
-                {query === "" && (
+                {query === '' && (
                   <div className="border-t border-gray-100 px-6 py-14 text-center text-sm sm:px-14">
                     <i className="fa-solid fa-magnifying-glass fa-2xl mx-auto h-6 w-6 text-gray-400"></i>
                     <p className="mt-4 font-semibold text-gray-900">
@@ -113,12 +113,12 @@ export default function MenuComponent({ open, setOpen }) {
                               value={item}
                               className={({ active }) =>
                                 classNames(
-                                  "cursor-default select-none px-4 py-2",
-                                  active && "bg-indigo-600 text-white",
+                                  'cursor-default select-none px-4 py-2',
+                                  active && 'bg-indigo-600 text-white'
                                 )
                               }
                             >
-                              {item.name.replace(/^[#>?]/, "")}
+                              {item.name.replace(/^[#>?]/, '')}
                             </Combobox.Option>
                           ))}
                         </ul>
@@ -127,7 +127,7 @@ export default function MenuComponent({ open, setOpen }) {
                   </Combobox.Options>
                 )}
 
-                {query !== "" && filteredItems.length === 0 && (
+                {query !== '' && filteredItems.length === 0 && (
                   <div className="border-t border-gray-100 px-6 py-14 text-center text-sm sm:px-14">
                     <i className="fa-solid fa-arrow-left mx-auto h-6 w-6 text-gray-400"></i>
                     <p className="mt-4 font-semibold text-gray-900">
@@ -141,31 +141,31 @@ export default function MenuComponent({ open, setOpen }) {
                 )}
 
                 <div className="flex flex-wrap items-center bg-gray-50 px-4 py-2.5 text-xs text-gray-700">
-                  Type{" "}
+                  Type{' '}
                   <kbd
                     className={classNames(
-                      "mx-1 flex h-5 w-5 items-center justify-center rounded border bg-white font-semibold sm:mx-2 border-indigo-600 text-indigo-600",
+                      'mx-1 flex h-5 w-5 items-center justify-center rounded border bg-white font-semibold sm:mx-2 border-indigo-600 text-indigo-600'
                     )}
                   >
                     #
-                  </kbd>{" "}
+                  </kbd>{' '}
                   <span className="sm:hidden">for projects,</span>
                   <span className="hidden sm:inline">to view projects,</span>
                   <kbd
                     className={classNames(
-                      "mx-1 flex h-5 w-5 items-center justify-center rounded border bg-white font-semibold sm:mx-2 border-gray-400 text-gray-900",
+                      'mx-1 flex h-5 w-5 items-center justify-center rounded border bg-white font-semibold sm:mx-2 border-gray-400 text-gray-900'
                     )}
                   >
                     &gt;
-                  </kbd>{" "}
-                  for pages, and{" "}
+                  </kbd>{' '}
+                  for pages, and{' '}
                   <kbd
                     className={classNames(
-                      "mx-1 flex h-5 w-5 items-center justify-center rounded border border-gray-400 bg-white font-semibold sm:mx-2",
+                      'mx-1 flex h-5 w-5 items-center justify-center rounded border border-gray-400 bg-white font-semibold sm:mx-2'
                     )}
                   >
                     ?
-                  </kbd>{" "}
+                  </kbd>{' '}
                   for help.
                 </div>
               </Combobox>
@@ -174,5 +174,5 @@ export default function MenuComponent({ open, setOpen }) {
         </div>
       </Dialog>
     </Transition.Root>
-  );
+  )
 }
