e5d57341230ee3efd1f22973969440502d36f4bb
Faye <faye@fayevr.dev>
2025-03-25 15:05:35 +0100

    remove server

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  server: {
-    hostname: '0.0.0.0',
-    port: 3000
-  },
   images: {
     remotePatterns: [
       {
